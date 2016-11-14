6e930017756c08202e856f5520ded865bfbc63e3
totond <yanzhikai_yjk@qq.com>
2016-11-15 01:31:28 +0800

    Initial commit

---
+++ app/src/main/java/scut/searchlayout/SearchLayout.java
@@ -0,0 +1,202 @@
+package scut.searchlayout;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.CursorAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Date;
+
+
+public class SearchLayout extends LinearLayout {
+    private Context context;
+    private EditText et_search;
+    private TextView tv_tip;
+    private ImageView iv_search;
+    private MyListView listView;
+    private TextView tv_clear;
+    private RecordSQLiteOpenHelper helper ;
+    private SQLiteDatabase db;
+    private BaseAdapter adapter;
+
+    public SearchLayout(Context context) {
+        super(context);
+        this.context = context;
+        init();
+    }
+
+    public SearchLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+        init();
+    }
+
+    public SearchLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.context = context;
+        init();
+    }
+
+    private void init(){
+        initView();
+        helper = new RecordSQLiteOpenHelper(context);
+
+        // 清空搜索历史
+        tv_clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                deleteData();
+                queryData("");
+            }
+        });
+
+        // 搜索框的键盘搜索键点击回调
+        et_search.setOnKeyListener(new View.OnKeyListener() {// 输入完后按键盘上的搜索键
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {// 修改回车键功能
+                    // 隐藏键盘，这里getCurrentFocus()需要传入Activity对象，如果实际不需要的话就不用隐藏键盘了，免得传入Activity对象，这里就先不实现了
+//                    ((InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+//                            getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                    // 按完搜索键后将当前查询的关键字保存起来,如果该关键字已经存在就不执行保存
+                    boolean hasData = hasData(et_search.getText().toString().trim());
+                    if (!hasData) {
+                        insertData(et_search.getText().toString().trim());
+                        queryData("");
+                    }
+                    // TODO 根据输入的内容模糊查询商品，并跳转到另一个界面，由你自己去实现
+                    Toast.makeText(context, "clicked!", Toast.LENGTH_SHORT).show();
+
+                }
+                return false;
+            }
+        });
+
+        // 搜索框的文本变化实时监听
+        et_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (s.toString().trim().length() == 0) {
+                    tv_tip.setText("搜索历史");
+                } else {
+                    tv_tip.setText("搜索结果");
+                }
+                String tempName = et_search.getText().toString();
+                // 根据tempName去模糊查询数据库中有没有数据
+                queryData(tempName);
+
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textView = (TextView) view.findViewById(android.R.id.text1);
+                String name = textView.getText().toString();
+                et_search.setText(name);
+                Toast.makeText(context, name, Toast.LENGTH_SHORT).show();
+                // TODO 获取到item上面的文字，根据该关键字跳转到另一个页面查询，由你自己去实现
+            }
+        });
+
+//        // 插入数据，便于测试，否则第一次进入没有数据怎么测试呀？
+//        Date date = new Date();
+//        long time = date.getTime();
+//        insertData("Leo" + time);
+
+        //点击搜索图片后的事件
+        iv_search.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean hasData = hasData(et_search.getText().toString().trim());
+                if (!hasData) {
+                    insertData(et_search.getText().toString().trim());
+                    queryData("");
+                }
+                // TODO 根据输入的内容模糊查询商品，并跳转到另一个界面，由你自己去实现
+                Toast.makeText(context, "clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 第一次进入查询所有的历史记录
+        queryData("");
+
+    }
+
+    private void initView(){
+        LayoutInflater.from(context).inflate(R.layout.search_layout,this);
+        et_search = (EditText) findViewById(R.id.et_search);
+        tv_clear = (TextView) findViewById(R.id.tv_clear);
+        tv_tip = (TextView) findViewById(R.id.tv_tip);
+        listView = (MyListView) findViewById(R.id.listView);
+        iv_search = (ImageView) findViewById(R.id.iv_search);
+
+    }
+
+    /**
+     * 插入数据
+     */
+    private void insertData(String tempName) {
+        db = helper.getWritableDatabase();
+        db.execSQL("insert into records(name) values('" + tempName + "')");
+        db.close();
+    }
+
+    /**
+     * 模糊查询数据
+     */
+    private void queryData(String tempName) {
+        Cursor cursor = helper.getReadableDatabase().rawQuery(
+                "select id as _id,name from records where name like '%" + tempName + "%' order by id desc ", null);
+        // 创建adapter适配器对象
+        adapter = new SimpleCursorAdapter(context, android.R.layout.simple_list_item_1, cursor, new String[] { "name" },
+                new int[] { android.R.id.text1 }, CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        // 设置适配器
+        listView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+    /**
+     * 检查数据库中是否已经有该条记录
+     */
+    private boolean hasData(String tempName) {
+        Cursor cursor = helper.getReadableDatabase().rawQuery(
+                "select id as _id,name from records where name =?", new String[]{tempName});
+        //判断是否有下一个
+        return cursor.moveToNext();
+    }
+
+    /**
+     * 清空数据
+     */
+    private void deleteData() {
+        db = helper.getWritableDatabase();
+        db.execSQL("delete from records");
+        db.close();
+    }
+}
